83ef096d41636dd8ee37dfb872b5448747fe9ded
stackrev <farbitherast5888@gmail.com>
2023-07-19 16:27:16 +0200

    connect wallet in clieint

---
+++ client/src/components/header.js
@@ -0,0 +1,10 @@
+"use client";
+import { Web3Button } from "@web3modal/react";
+
+export default function Header() {
+  return (
+    <div className="flex justify-end ">
+      <Web3Button />
+    </div>
+  );
+}
